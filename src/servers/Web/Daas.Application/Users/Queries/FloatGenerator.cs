24c99746be7559780bf545ce8d94d51aa04fed10
Akash C <121154000+akashc15@users.noreply.github.com>
2026-02-25 08:34:31 +0530

    Add files via upload
    Added a service where it generates the random float data

---
+++ src/servers/Web/Daas.Application/Users/Queries/FloatGenerator.cs
@@ -0,0 +1,19 @@
+﻿namespace Daas.Application.Users.Queries;
+
+public class FloatGenerator: IFieldValueGenerator
+
+{
+    private Random _random;
+    public FloatGenerator(Random random)
+    {
+        _random = random;
+    }
+
+    public object Generator()
+    {
+        float min = 0.0f;
+        float max = 2000.0f;
+        float randomfloat = (float)(_random.NextDouble()*(max-min) + min); 
+        return randomfloat;
+    }
+}
