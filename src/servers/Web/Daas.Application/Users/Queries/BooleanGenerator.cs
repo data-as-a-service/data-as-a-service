18cb49299e8587908cbf321e23dcde27a3bc5bb3
Akash C <121154000+akashc15@users.noreply.github.com>
2026-02-26 08:29:16 +0530

    Added a boolean generator service

---
+++ src/servers/Web/Daas.Application/Users/Queries/BooleanGenerator.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Daas.Application.Users.Queries
+{
+    public class BooleanGenerator : IFieldValueGenerator
+    {
+        private Random _random;
+        public BooleanGenerator(Random random) {
+            _random = random;
+        }
+        public object Generator()
+        {
+            byte randombyte = (byte) _random.Next(0, 256);
+            return Convert.ToString(randombyte , 2).PadLeft(8, '0');
+
+        }
+
+      }
+}
