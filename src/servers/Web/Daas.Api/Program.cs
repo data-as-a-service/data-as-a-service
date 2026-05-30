3f01da60dd73d70b2faa987cc65804ec3f6e8c44
sooraj <soorajsm56@gmail.com>
2026-05-30 17:29:40 +0530

    fixing the comment

--- src/servers/Web/Daas.Api/Program.cs
+++ src/servers/Web/Daas.Api/Program.cs
@@ -84,7 +84,7 @@ var app = builder.Build();
 app.UseHttpsRedirection();
 app.MapControllers();
 
-# This ensures the database is created and migrations are applied on startup.
+// This ensures the database is created and migrations are applied on startup.
 
 using (var scope = app.Services.CreateScope())
 {
