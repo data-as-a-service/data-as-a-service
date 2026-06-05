29abe86b99b75f069b2bc9b89e4517a7cac2c314
Chinthamani Sai Krishna <saikrishnaias2004@gmail.com>
2026-06-06 00:32:52 +0530

    DAAS - Add schema creation frontend and Integrate API

--- src/servers/Web/Daas.Api/wwwroot/Script.js
+++ src/servers/Web/Daas.Api/wwwroot/Script.js
@@ -74,3 +74,90 @@ async function generateData(id) {
     ).textContent =
         JSON.stringify(data, null, 2);
 }
+
+function addField() {
+    const container =
+        document.getElementById(
+            "fieldsContainer"
+        );
+
+    container.innerHTML += `
+        <div class="row mb-2">
+
+            <div class="col">
+                <input
+                    class="form-control fieldName"
+                    placeholder="Field Name">
+            </div>
+
+            <div class="col">
+                <select
+                    class="form-select fieldType">
+                    <option value="0">INT</option>
+                    <option value="1">Float</option>
+                    <option value="2">Boolean</option>
+                    <option value="3">String</option>
+                    <option value="4">Character</option>
+                    <option value="5">GUID</option>
+                    <option value="6">Date</option>
+                    <option value="7">Double</option>
+                </select>
+            </div>
+
+        </div>
+    `;
+}
+
+addField();
+
+async function createSchema() {
+    const schemaName =
+        document.getElementById(
+            "schemaName"
+        ).value;
+
+    const fieldNames =
+        document.querySelectorAll(
+            ".fieldName"
+        );
+
+    const fieldTypes =
+        document.querySelectorAll(
+            ".fieldType"
+        );
+
+    const fields = [];
+
+    for (let i = 0; i < fieldNames.length; i++) {
+        fields.push({
+            fieldName:
+                fieldNames[i].value,
+
+            fieldType:
+                parseInt(
+                    fieldTypes[i].value
+                )
+        });
+    }
+
+    const schema = {
+        name: schemaName,
+        fields: fields
+    };
+
+    await fetch(
+        "/api/schema",
+        {
+            method: "POST",
+
+            headers: {
+                "Content-Type":
+                    "application/json"
+            },
+
+            body:
+                JSON.stringify(schema)
+        });
+
+    loadSchemas();
+}
