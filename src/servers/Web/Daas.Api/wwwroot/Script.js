2c912469f8c784878ad181e8bd96519d9eb9b0b0
Chinthamani Sai Krishna <saikrishnaias2004@gmail.com>
2026-06-05 23:35:55 +0530

    DAAS - Implement Load Schema logic

--- src/servers/Web/Daas.Api/wwwroot/Script.js
+++ src/servers/Web/Daas.Api/wwwroot/Script.js
@@ -6,7 +6,39 @@
     const schemas =
         await response.json();
 
-    console.log(schemas);
+    const container =
+        document.getElementById("schemas");
+
+    container.innerHTML = "";
+
+    schemas.forEach(schema => {
+
+        container.innerHTML += `
+            <div class="card mb-2">
+                <div class="card-body">
+
+                    <h5>${schema.name}</h5>
+
+                    <p>${schema.fields.length} fields</p>
+
+                    <button
+                        class="btn btn-primary"
+                        onclick="generateData('${schema.id}')">
+
+                        Generate Data
+                    </button>
+
+                    <button
+                        class="btn btn-danger"
+                        onclick="deleteSchema('${schema.id}')">
+
+                        Delete
+                    </button>
+
+                </div>
+            </div>
+        `;
+    });
 }
 
 loadSchemas();
