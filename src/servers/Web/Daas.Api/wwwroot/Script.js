8366a08e4c30950571df104208dbc96a27768ae8
Chinthamani Sai Krishna <saikrishnaias2004@gmail.com>
2026-06-05 21:28:06 +0530

    DAAS - Add Script file

---
+++ src/servers/Web/Daas.Api/wwwroot/Script.js
@@ -0,0 +1,12 @@
+﻿async function loadSchemas() {
+
+    const response =
+        await fetch('/api/schema');
+
+    const schemas =
+        await response.json();
+
+    console.log(schemas);
+}
+
+loadSchemas();
